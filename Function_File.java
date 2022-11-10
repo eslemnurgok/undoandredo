9b4457baf1bc9e0b897fb06ae0ae694907ca44bb
eslemnurgok <99400363+eslemnurgok@users.noreply.github.com>
2022-11-10 22:10:32 +0300

    Create Function_File.java

---
+++ Function_File.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guı;
+
+
+public class Function_File {
+    GUI gui;
+
+    public Function_File(GUI gui){
+        this.gui=gui;
+        
+    }
+    
+}
